3059a940d1e16632e9d973ea61af533a68629e5b
EC2 Default User <ec2-user@ip-172-31-32-209.eu-west-1.compute.internal>
2024-11-25 12:36:34 +0000

    static files loaded

--- adminproject/settings.py
+++ adminproject/settings.py
@@ -114,19 +114,25 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
+# https://docs.djangoproject.com/en/4.2/topics/i18n/
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'US/Pacific'
 
 USE_I18N = True
+
+USE_L10N = True
 
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# URL for serving static files
+STATIC_URL = '/static/'
+
+# Directory to collect static files in production
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
